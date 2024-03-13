4cfb87c1451057e65b33f99ffcb20f63e6052da4
smk762 <smk762@iinet.net.au>
2024-03-14 02:24:38 +0800

    weave it together

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # nucleus-explorer
+
+- launch the docker PGSQL container with `docker-compose build && docker-compose up pgsqldb`
+- Enter the container with `docker-compose run pgsqldb bash` 
+- Setup PGSQL following the guide at https://docs.bigdipper.live/cosmos-based/parser/database
+
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source .env
+
+# make sure you select the correct branch inside the BDJuno repository to view the correct schema for the chain you want to parse.
+HOST_SQL_FOLDER="callisto/database/schema"
+DOCKER_SQL_FOLDER="/sql_schema"
+
+# Iterate over each SQL file in the folder
+for sql_file in "$HOST_SQL_FOLDER"/*.sql; do
+    if [ -f "$sql_file" ]; then
+        fn=$(basename "$sql_file")
+        # Execute the SQL file using Docker Compose
+        docker-compose exec pgsqldb psql -U $POSTGRES_USER -d bdjuno -f "${DOCKER_SQL_FOLDER}/${fn}"
+        
+        # Check the exit status
+        if [ $? -ne 0 ]; then
+            echo "Error executing $fn"
+            exit 1
+        else
+            echo "Successfully executed $fn"
+        fi
+    fi
+done
