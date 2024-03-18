dd544c807f77f9d4474072a8412b2d22643e0cf8
smk762 <smk762@iinet.net.au>
2024-03-19 03:13:30 +0800

    create setup script to do all the things

--- setup.sh
+++ setup.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
+
+# Get Environment Variables
 source .env
 
-# make sure you select the correct branch inside the BDJuno repository to view the correct schema for the chain you want to parse.
+# Setup for nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Generate front end
+cd big-dipper-2.0-cosmos
+nvm use 18
+yarn
+corepack enable
+yarn build --filter web-nucleus
+cd -
+
+# Build and start the Docker containers
+docker compose stop
+docker compose down -v && docker-compose down -v
+docker compose build && docker compose up -d
+
+# Apply the database schema
 HOST_SQL_FOLDER="callisto/database/schema"
 DOCKER_SQL_FOLDER="/sql_schema"
-
-docker compose up -d pgsqldb
 sleep 5
-
-# Iterate over each SQL file in the folder
 for sql_file in "$HOST_SQL_FOLDER"/*.sql; do
     if [ -f "$sql_file" ]; then
         fn=$(basename "$sql_file")
-        # Execute the SQL file using Docker Compose
         docker-compose exec pgsqldb psql -U $POSTGRES_USER -d bdjuno -f "${DOCKER_SQL_FOLDER}/${fn}"
-        
-        # Check the exit status
         if [ $? -ne 0 ]; then
             echo "Error executing $fn"
             docker-compose stop pgsqldb
@@ -25,4 +38,12 @@ for sql_file in "$HOST_SQL_FOLDER"/*.sql; do
         fi
     fi
 done
-docker-compose stop pgsqldb
+
+# Apply Hasura metadata
+curl -L https://github.com/hasura/graphql-engine/raw/stable/cli/get.sh | bash
+cd callisto/hasura
+hasura metadata apply --endpoint http://localhost:8080 --admin-secret myadminsecretkey
+cd -
+
+echo "Use the following command to follow the container logs:"
+echo "docker compose logs -f -n 10"
